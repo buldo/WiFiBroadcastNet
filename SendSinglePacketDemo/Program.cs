82db42cac8a5d265cec8db00938a77af5a68a18f
Roman Buldygin <froooks@gmail.com>
2023-08-24 01:06:26 +0300

    save

--- SendSinglePacketDemo/Program.cs
+++ SendSinglePacketDemo/Program.cs
@@ -6,62 +6,22 @@ using Bld.Libnl.Net.Nl80211.Enums;
 
 using RunProcessAsTask;
 
-namespace SendSinglePacketDemo
+using SharpPcap.LibPcap;
+
+namespace SendSinglePacketDemo;
+
+internal class Program
 {
-    internal class Program
+    /// <summary>
+    /// sudo iw wlan1 set monitor otherbss
+    /// sudo iw wlan1 set channel 48
+    /// </summary>
+    static async Task Main(string[] args)
     {
-        static async Task Main(string[] args)
+        var devices = LibPcapLiveDeviceList.Instance.ToList();
+        foreach (var device in devices)
         {
-            var ifName = "wlan1";
-            var runResult = await ProcessEx.RunAsync("iw", $"{ifName} set monitor otherbss");
-            Console.WriteLine(runResult.StandardOutput.Concat(runResult.StandardError));
-            //return string.Join(Environment.NewLine, runResult.StandardOutput.Concat(runResult.StandardError));
-        }
-    }
-
-
-    internal static class LinuxDevicesHelper
-    {
-        public static List<NlWiPhy> GetWirelessPhy()
-        {
-            var interfaces = new List<NlWiPhy>();
-            var nl80211Wrapper = new Nl80211Wrapper();
-            var dumpMessages = nl80211Wrapper.DumpWiphy();
-            foreach (var message in dumpMessages)
-            {
-                var name = (string)message.Attributes[nl80211_attrs.NL80211_ATTR_WIPHY_NAME].GetValue();
-                var wiPhy = (UInt32)message.Attributes[nl80211_attrs.NL80211_ATTR_WIPHY].GetValue();
-                var modes = (IfMode[])message.Attributes[nl80211_attrs.NL80211_ATTR_SUPPORTED_IFTYPES].GetValue();
-                var iface = new NlWiPhy()
-                {
-                    WiPhyName = name,
-                    WiPhy = wiPhy,
-                    Modes = modes
-                };
-                interfaces.Add(iface);
-            }
-
-            return interfaces;
-        }
-
-        public static List<NlInterface> GetWirelessInterfaces()
-        {
-            var interfaces = new List<NlInterface>();
-            var nl80211Wrapper = new Nl80211Wrapper();
-            var dumpMessages = nl80211Wrapper.DumpInterfaces();
-            foreach (var message in dumpMessages)
-            {
-                var ifName = (string)message.Attributes[nl80211_attrs.NL80211_ATTR_IFNAME].GetValue();
-                var wiPhy = (UInt32)message.Attributes[nl80211_attrs.NL80211_ATTR_WIPHY].GetValue();
-                var iface = new NlInterface
-                {
-                    WiPhy = wiPhy,
-                    InterfaceName = ifName
-                };
-                interfaces.Add(iface);
-            }
-
-            return interfaces;
+            Console.WriteLine($"{device.Name}");
         }
     }
 }

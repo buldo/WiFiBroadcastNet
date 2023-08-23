76688fa43a28f08bfd25ae6586bb494539c3b455
Roman Buldygin <froooks@gmail.com>
2023-08-24 00:32:12 +0300

    Save

---
+++ SendSinglePacketDemo/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SendSinglePacketDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

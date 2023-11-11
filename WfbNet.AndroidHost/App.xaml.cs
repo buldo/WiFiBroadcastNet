3b55c5a53c3cf526c0fc8fea1ae5cc696d5cad6f
Roman Buldygin <froooks@gmail.com>
2023-11-11 04:28:59 +0300

    NewProject

---
+++ WfbNet.AndroidHost/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace WfbNet.AndroidHost;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new AppShell();
+    }
+}
---
+++ WfbNet.AndroidHost/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace WfbNet.AndroidHost;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
